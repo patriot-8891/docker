d650df95a8054be16a8f2d4afd36d7221acb5de6
Евгений Макаров <patriot-8891@ya.ru>
2026-02-08 14:47:18 +0500

    Add uwsgi-pwsh

---
+++ uwsgi-pwsh/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# /etc/apk/repositories
+cat > /etc/apk/repositories << EOF
+https://mirror.yandex.ru/mirrors/alpine/v3.23/main
+https://mirror.yandex.ru/mirrors/alpine/v3.23/community
+EOF
+
+apk update
+apk upgrade
+apk add coreutils-env libldap powershell uwsgi-cgi uwsgi-ugreen
+ln -s /usr/lib/libldap.so.2 /usr/local/lib/libldap-2.5.so.0.so
